8807731b7e0e54853cebd02da87aa77095ee3d63
Roberto Zanartu Gomez <rzanartu@icci.cl>
2013-05-26 18:01:26 -0400

    ultimos cambios

--- index.php
+++ index.php
@@ -10,35 +10,29 @@
 
 include('lib/nusoap.php');
 
-$rut = $_POST['rut'];
-$password = $_POST['password'];
+$rut = $_POST["rut"];
+$password = $_POST["password"];
 
 $password = strtoupper($password);
 $password = hash("SHA256", $password);
- 
-$rut = str_replace('.', '-', $rut); 
- 
+
 
 $parametros = array();
-$parametros['rut'] = $rut;
-$parametros['password'] = $password;
+$parametros["rut"] = $rut;
+$parametros["password"] = $password;
 
 $objClienteSOAP = new soapclient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl");
 $objRespuesta = $objClienteSOAP->autenticar($parametros);
 
 //var_dump($objRespuesta);
 
-
-$codigo = (int) $objRespuesta->return->codigo;
-$res = (string) $objRespuesta->return->descripcion;
-
+$codigo =  $objRespuesta->return->codigo;
+$descripcion =  $objRespuesta->return->descripcion;
 
 if ($codigo == 1) 
 	echo "<h1>¡Bienvenido(a)!</h1>";
  else 
 	echo "<h1>¡Rechazado!</h1>";
-
 ?>
 </body>
-
 </html>
